5790ba366e76b9627dff78c7e9431e4fa66e7a66
Heitor Saldanha <heitorsaldanha@gmail.com>
2020-09-13 17:40:32 +0100

    Conclusão do Curso

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,28 @@
 const Atendimento = require('../models/atendimentos');
+const atendimentos = require('../models/atendimentos');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('GET Atendimentos'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res);
+    });
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.buscaPorId(id, res);
+    });
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
         Atendimento.adiciona(atendimento,res);
-    })}
+    });
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.altera(id, req.body, res);
+    });
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Atendimento.remove(id, res);
+    });
+}
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -30,10 +30,63 @@ class Atendimento {
         if(err) {
           res.status(400).json(err);
         } else {
-          res.status(201).json(result);
+          res.status(201).json({atendimentoDatado});
         }
       });
     }
+  }
+
+  lista(res){
+    const sql = `
+      SELECT * FROM Atendimentos
+    `;
+
+    conexao.query(sql, (err, result) => {
+      if(err) {
+        res.status(400).json(err)
+      } else {
+        res.status(200).json(result)
+      }
+    });
+  }
+
+  buscaPorId(id, res) {
+    const sql = `
+      SELECT * FROM Atendimentos WHERE id=${id}
+    `;
+
+    conexao.query(sql, (err, result) => {
+      if(err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json(result[0]);
+      }
+    })
+  }
+
+  altera(id, valores, res) {
+    if(valores.data) {
+      valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    }
+    const sql = 'UPDATE Atendimentos set ? WHERE id=?';
+    conexao.query(sql, [valores,id], (err, result) => {
+      if(err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json({...valores, id});
+      }
+    });
+  }
+
+  remove(id, res) {
+    const sql = `DELETE FROM Atendimentos WHERE id=${id}`;
+    conexao.query(sql, (err, result) => {
+      if(err) {
+        res.status(400).json(err);
+      } else {
+        res.status(200).json({id});
+      }
+    });
   }
 }
 
